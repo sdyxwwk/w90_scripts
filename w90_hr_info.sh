9e1c2a4cbc74ddb287db372b77c70b8676ae4b0a
Keith <keith@KeithMacBook.local>
2026-04-28 09:24:32 +0800

    add w90_hr_info.sh

--- w90_hr_info.sh
+++ w90_hr_info.sh
@@ -15,21 +15,18 @@ hrfile=$1
 #------------------------------------------------
 # Print number of Wannier functions
 #------------------------------------------------
-head -2 ${hrfile} | tail -1 | awk '{printf "Number of Wannier functions => %6d\n
-", $1}'
+head -2 ${hrfile} | tail -1 | awk '{printf "Number of Wannier functions => %6d\n", $1}'
 
 #------------------------------------------------
 # Print number of R-vectors
 #------------------------------------------------
-head -3 ${hrfile} | tail -1 | awk '{printf "Number of R-vectors         => %6d\n
-", $1}'
+head -3 ${hrfile} | tail -1 | awk '{printf "Number of R-vectors         => %6d\n", $1}'
 
 #------------------------------------------------
 # Print On-site terms
 #------------------------------------------------
 echo ""
 echo "On-site terms:"
-grep "^    0    0    0" ${hrfile} | awk '{if($4==$5){printf "%3d => %7.3f\n", $4
-,$6}}'
+grep "^    0    0    0" ${hrfile} | awk '{if($4==$5){printf "%3d => %7.3f\n", $4, $6}}'
 
 echo "========================================"
